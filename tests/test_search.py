a17e3dc89a4b3a200af7eed513f6d0815772c5d1
Zhao Xiaohong <zhaoxiaohong@metasota.ai>
2024-11-27 16:43:16 +0800

    删除 test_search_with_topic
    test_subject 已经测试了带 topic 的 search。

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,4 +1,4 @@
-from metaso_sdk.model import Query, Topic
+from metaso_sdk.model import Query
 from metaso_sdk.search import search
 
 
@@ -15,13 +15,3 @@ def test_search_stream():
     question = "42"
     query = Query(question=question)
     assert question in "".join(chunk["text"] for chunk in search(query, stream=True) if chunk["type"] == "append-text")
-
-
-def test_search_with_topic():
-    question = "函数"
-    topic = Topic(id="8549231270939611136", name="XYZ")
-    query = Query(question=question)
-    result = search(query, topic=topic)
-    assert "resultId" in result
-    assert "references" in result
-    assert question in result["text"]
